3dc48ab89eaab9be0cda5aa07421053c656a96ef
t-jenson-n <tj.nykilchyk@gmail.com>
2026-03-05 16:25:39 -0500

    ROCK COUNTER VISIBLE

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using UnityEngine.UI;
+using TMPro;
 
 
 public class GameController : MonoBehaviour
@@ -11,9 +12,11 @@ public class GameController : MonoBehaviour
     public int[] gemCollect = { 1, 2, 3, 4 };
     public bool[] gemHas = { false, false, false, false };
 
-    public int rocksCollected;
+    public int rocksCollected = 0;
 
     public Button backpackButton;
+
+    public TextMeshProUGUI rocksCounter_Text;
 
     public bool melodyCorrect = false;
 
@@ -21,11 +24,12 @@ public class GameController : MonoBehaviour
     {
         backpackButton.GetComponent<StatusChange>().Hide();
 
-
     }
 
     void Update()
     {
+        rocksCounter_Text.text = "Rocks Collected: " + rocksCollected;
+
         if (rocksCollected >= 4)
         {
             backpackButton.GetComponent<StatusChange>().Show();
