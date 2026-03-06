7c07b0668bf17d2720ba2745515964f3fdca9c01
Petri124 <portia.s.bridge@gmail.com>
2026-03-05 19:34:36 -0500

    uhhhh

--- Assets/Scripts/AudioPlay.cs
+++ Assets/Scripts/AudioPlay.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,7 @@ public class AudioPlay : MonoBehaviour
 {
     public GameObject[] parentbars; //parent
     public Button playButton;
+  
     private GameObject[] bars = new GameObject[24]; //child
     private GameObject[,] bars2 = new GameObject[4,6];
     // Start is called before the first frame update
@@ -16,20 +18,16 @@ public class AudioPlay : MonoBehaviour
         {
             for(int j=0; j < 6; j++)
             {
-                //bars[(i*6)+j] = parentbars[i].transform.GetChild(j).gameObject;
-                int val = (i * 6) + j;
-                bars[val] = parentbars[i].transform.GetChild(j).gameObject;
+               
+                //int val = (i * 6) + j;
+                //bars[val] = parentbars[i].transform.GetChild(j).gameObject;
                 bars2[i,j] = parentbars[i].transform.GetChild(j).gameObject;
                // Debug.Log("val"+val+": "+parentbars[i].transform.GetChild(j).gameObject.name);
             }
         }
         
-        for(int i = 0;i < 4; i++) {
+       /* for(int i = 0;i < 4; i++) {
             Debug.Log("0:"+bars2[i,0].name+", 1:"+ bars2[i,1].name + ", 2:"+bars2[i,2].name+", 3"+bars2[i,3].name+", 4"+bars2[i,4].name+", 5"+bars2[i,5].name);
-        }
-       /* for(int i = 0;i < bars.Length; i++)
-        {
-            Debug.Log(i+": "+bars[i].name+", ");
         }*/
     }
 
@@ -43,5 +41,19 @@ public class AudioPlay : MonoBehaviour
 
             }
         }*/
+    }
+
+    void getSequence()
+    {
+        for( int i = 0;i < 6; i++)
+        {
+            for(int j=0; j< 4; j++)
+            {
+                if (bars2[j, i].GetComponent<Collider>().isTrigger == true)
+                {
+                   
+                }
+            }
+        }
     }
 }
